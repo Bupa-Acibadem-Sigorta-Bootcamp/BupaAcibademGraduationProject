b3317b3261b73637c86b344002585f3983cae016
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-02-05 22:41:25 +0300

    Api controller objects created and implemented

---
+++ HealthInsurance.WebApiLayer/Controllers/Controller/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthInsurance.EntityLayer.Concrete.Dtos;
+using HealthInsurance.EntityLayer.Concrete.Models;
+using HealthInsurance.InterfaceLayer.Abstract.IModelService;
+using HealthInsurance.WebApiLayer.Controllers.ControllerBases;
+
+namespace HealthInsurance.WebApiLayer.Controllers.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : BasesController<IOrderService, Order, DtoOrder>
+    {
+        public OrderController(IOrderService service) : base(service)
+        {
+        }
+    }
+}
